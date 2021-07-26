fb93511b2cb0e06641449aae71d5a31035401b36
Lucian <luciantrestian007@gmail.com>
2021-07-26 21:23:01 +0300

    new

--- sort odd number and save even position.py
+++ sort odd number and save even position.py
@@ -1,12 +1,20 @@
 def sort_array(source_array):
     splited_array = list(map(int, ''.join([i for i in source_array.split(',')]).split()))   # create clear list with integer elemenets 
-    odd_array = sorted([i for i in splited_array if i % 2 != 0])
-    for i in range(0, len(splited_array)):
-        n = 0
-        if len(odd_array) != 0 or len(splited_array) != 0:
-            sorted_array = [splited_array[i] == "" for i in splited_array if i in odd_array]
-
-
-    return sorted_array
+    odd_array = sorted([i for i in splited_array if i % 2 != 0])    # extract odd numbers
+    for index, item in enumerate(splited_array):    #try to insert in splited_array
+        if item in odd_array:
+            splited_array[index] = odd_array.pop(0)
+    return splited_array
 
 print(sort_array(input()))
+
+#def sort_array(source_array):
+#   result = sorted([l for l in source_array if l % 2 == 1])
+#    for index, item in enumerate(source_array):
+#        if item % 2 == 0:
+#            result.insert(index, item)
+#    return result
+
+#def sort_array(source_array):
+#   odds = iter(sorted(v for v in source_array if v % 2))
+#    return [next(odds) if i % 2 else i for i in source_array]
