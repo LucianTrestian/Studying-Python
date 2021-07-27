8b06a6afda6248d944fe09b60b60f4140b5d1dc9
Lucian <luciantrestian007@gmail.com>
2021-07-27 20:50:13 +0300

    27/7/2021

--- Human Readable Time.py
+++ Human Readable Time.py
@@ -4,3 +4,5 @@ def make_readable(seconds):
     return f'{hours:02d}:{min:02d}:{seconds:02d}'
 
 print(make_readable(int(input())))
+
+#return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60) # one line
