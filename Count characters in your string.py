dbcf4a55c01862cf7366f6a673062bdf454f2f24
Lucian <luciantrestian007@gmail.com>
2021-07-30 17:02:05 +0300

    30/7/2021

--- Count characters in your string.py
+++ Count characters in your string.py
@@ -1,4 +1,12 @@
 def count(string):
     string_count = list(dict.fromkeys(list(string)))    #converts string in list with double caracters for easy count
     return dict([(string_count[i], string.count(string_count[i])) for i in range(len(string_count))])
-    #create a dictionary if caracters and counts
+    #create a dictionary if characters and counts
+
+#from collections import Counter
+#def count(string):
+#    return Counter(string)
+
+#def count(string):
+#  
+#    return {i: string.count(i) for i in string}
