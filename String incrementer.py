79463e4337b2fe95f743528d9209ad174ff9a80e
Lucian <luciantrestian007@gmail.com>
2021-08-05 22:36:50 +0300

    8/5/2021

--- String incrementer.py
+++ String incrementer.py
@@ -1,8 +1,26 @@
-import re
+def increment_string(s):
+    if s and s[-1].isdigit():
+        return increment_string(s[:-1]) + "0" if s[-1] == "9" else s[:-1] + `int(s[-1]) + 1`
+    return s + "1"
 
-def increment_string(strng):
-    return re.sub(r'[0-9]+$',
-                lambda x: f"{str(int(x.group())+1).zfill(len(x.group()))}", 
-                strng)
-
-print(increment_string(input()))
+#def increment_string(strng):
+#    
+#    #strip the decimals from the right
+#    stripped = strng.rstrip('1234567890')
+#    
+#    # get the part of strng that was stripped
+#    ints = strng[len(stripped):]
+#    
+#    if len(ints) == 0:
+#        return strng + '1'
+#    else:
+#        # find the length of ints
+#        length = len(ints)
+#    
+#        # add 1 to ints
+#        new_ints = 1 + int(ints)
+#    
+#        # pad new_ints with zeroes on the left
+#        new_ints = str(new_ints).zfill(length)
+#    
+#        return stripped + new_ints
